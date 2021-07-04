3ad4fb6cf29101f805eb05bdddfefd7a550ae0ab
Yuhao Wu <wyhfrank@gmail.com>
2021-07-04 17:57:38 +0000

    Update analyser to analyse earn rate.

--- analyser.py
+++ analyser.py
@@ -1,11 +1,18 @@
+import sys
+sys.path.append('.')
+import datetime
 import pandas as pd
 import python_bitbankcc
-from utils import read_config
+from utils import create_pine_script, read_config
 
 
 MAX_ORDER_HISTORY_COUNT = 99999999
 
 def check_avg(df, side):
+    start = df['executed_at_date'].min()
+    end = df['executed_at_date'].max()
+    count = df.shape[0]
+
     df_side = df[df['side']==side]
     df_cost = df_side.price * df_side.amount
     total_cost = df_cost.sum()
@@ -16,6 +23,9 @@ def check_avg(df, side):
         'total_cost': total_cost,
         'total_amount': total_amount,
         'avg_cost': avg_cost,
+        'start': start,
+        'end': end,
+        'count': count,
     }
     return avg
 
@@ -38,29 +48,95 @@ def convert_date(df, cols=['executed_at']):
         df[f"{col}_date"] = pd.to_datetime(df[col], unit='ms', utc=True).dt.tz_convert('Asia/Tokyo')
 
 
+def analyze_earn_rate(df, latest_price):
+    df = df[df['cost'] > 5000]
+    res = {}
+    for side in ['buy', 'sell']:
+        res[side] = check_avg(df, side)
+    
+    buy_cost = res['buy']['total_cost']
+    sell_cost = res['sell']['total_cost']
+    current_amount = res['buy']['total_amount'] - res['sell']['total_amount']
+    pivot_price = (buy_cost - sell_cost) / current_amount
+    res['pivot_price'] = pivot_price
+
+    res['earn_rate'] = (sell_cost + current_amount * latest_price - buy_cost) / buy_cost
+
+    return res
+
+
+def get_pine_script(df, symbol):
+    code = create_pine_script(df)
+    with open(f"data/{symbol}.script.txt", 'w') as f:
+        f.write(code)
+
+
 def get_trade_history(exchange, symbols):
-    dfs = []
+    dfs = pd.DataFrame()
     for symbol in symbols:
+        pub = python_bitbankcc.public()
+        resp = pub.get_ticker(pair=symbol)
+        latest_price = float(resp['last'])
+
         resp = exchange.get_trade_history(pair=symbol, order_count=MAX_ORDER_HISTORY_COUNT)
         data = resp['trades']
+        
         df = pd.DataFrame(data)
         convert_float(df)
+        df['cost'] = df['amount'] * df['price']
         convert_date(df)
-        print(df.head())
-        dfs.append(df)
+
+        res = analyze_earn_rate(df, latest_price)
+
+        get_pine_script(df[df['cost']>2000], symbol=symbol)
+        print(symbol, res)
+        # print(res)
+        # print(df.head())
+
+        dfs = dfs.append(df)
     return dfs
 
 
-def main():
+def get_candlesticks(exchange, symbols, window='1min', date=datetime.datetime.now()-datetime.timedelta(days=1)):
+    date_str = date.strftime('%Y%m%d')
+    date_str
+    dfs = {}
+    for symbol in symbols:
+        resp = exchange.get_candlestick(pair=symbol, candle_type=window, yyyymmdd=date_str)
+        data = resp['candlestick'][0]['ohlcv']
+        df = pd.DataFrame(data)
+        df.columns = ['open', 'high', 'low', 'close', 'volume', 'timestamp']
+        convert_float(df, cols=['open', 'high', 'low', 'close', 'volume',])
+        convert_date(df, cols=['timestamp'])
+        df['height'] = df['high'] - df['low']
+        print(df.head())
+
+        print(df['height'].describe())
+
+        dfs[symbol] = df
+    return dfs
+
+
+def analyze_trade_history():
     config = read_config()
     API_KEY = config['api']['key']
     API_SECRET = config['api']['secret']
-    symbols = ['btc_jpy']
+    symbols = ['btc_jpy', 'eth_jpy']
 
     exchange = python_bitbankcc.private(api_key=API_KEY, api_secret=API_SECRET)
     df_trades = get_trade_history(exchange=exchange, symbols=symbols)
-    
+    df_trades.to_csv('data/trades.csv')
+
+
+def analyze_candle_height():
+    symbols = ['eth_jpy']
+    window = '5min'
+    exchange = python_bitbankcc.public()
+
+    df_candlesticks = get_candlesticks(exchange=exchange, symbols=symbols, window=window)
 
 
 if __name__ == "__main__":
-    main()
+    analyze_trade_history()
+    # analyze_candle_height()
+
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import asyncio
 import yaml
 
@@ -17,3 +18,37 @@ def read_config(fn='./configs/config.yml'):
     with open(fn, 'r') as f:
         config = yaml.safe_load(f)
     return config
+
+
+def create_pine_script(df_history, side_key='side', cost_key='cost', time_key='executed_at', script_title="History", max_lines=200):
+    lines = [
+        "// @version=4\n\nstudy(\"{}\", overlay=true, max_labels_count=500)\n".format(script_title),
+    ]
+
+    count = 0
+    for i, row in df_history.iterrows():
+
+        count += 1
+        if count > max_lines:
+            break
+
+        if row[side_key] == "sell":
+            color = "color.red"
+            text = "Sell\\n"
+            style = 'label.style_labeldown'
+            yloc = 'yloc.abovebar'
+        elif row[side_key] == "buy":
+            color = "color.green"
+            text = "Buy\\n"
+            style = 'label.style_labelup'
+            yloc = 'yloc.belowbar'
+        text += "{0:.0f}".format(row[cost_key])
+
+        time = datetime.fromtimestamp(row[time_key]/1000)
+        line = 'label.new(timestamp("GMT+9",{time}),close,xloc=xloc.bar_time,' \
+               'yloc={yloc},text="{text}",style={style},color={color})' \
+            .format(time=",".join(map(str, [time.year, time.month, time.day, time.hour, time.minute])),
+                    text=text, color=color, style=style, yloc=yloc)
+        lines.append(line)
+
+    return "\n".join(lines)
