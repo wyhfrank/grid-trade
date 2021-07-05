c270ff50f6832507250deab1aeb341f2eac8b240
Yuhao Wu <wyhfrank@gmail.com>
2021-07-05 12:30:55 +0000

    Print stack trace as well in exception.

--- notification/webhook.py
+++ notification/webhook.py
@@ -1,22 +1,31 @@
+import sys
 import json
+import traceback
 import requests
 
 
 class Discord:
-    def __init__(self, info_webhook, err_webhook, also_print=True) -> None:
+    def __init__(self, info_webhook, err_webhook, also_print=True, no_http=False) -> None:
         self.info_webhook = info_webhook
         self.err_webhook = err_webhook
         self.also_print = also_print
+        self.no_http = no_http
 
     def info(self, message):
         self.send(message=message, info_type='info')
 
-    def error(self, message):
-        self.send(message=message, info_type='error')
+    def error(self, message, include_traceback=True):
+        tb = ""
+        if include_traceback:
+            tb = self.get_traceback_message()
+        self.send(message=str(message) + tb, info_type='error')
 
     def send(self, message, info_type='info'):
         if self.also_print:
             print(message)
+        
+        if self.no_http:
+            return
         body = {}
         if info_type == "info":
             url = self.info_webhook
@@ -27,4 +36,40 @@ class Discord:
         headers = {
             'Content-Type': 'application/json',
         }
-        requests.request("POST", url, headers=headers, data=payload) 
+        requests.request("POST", url, headers=headers, data=payload)
+
+    @staticmethod
+    def get_traceback_message():
+        _, _, tb = sys.exc_info()
+        if not tb:
+            return ""
+        lines = traceback.format_tb(tb)
+        tb_str = "".join(lines).strip()
+        # Code block im markdown format
+        return f"\n```{tb_str}```"
+
+
+##################
+# Tests
+def load_discord():
+    import sys
+    sys.path.append('.')
+    from utils import read_config
+    config = read_config()
+    discord_info_webhook = config['discord']['test']
+    discord_error_webhook = config['discord']['test']
+    discord = Discord(info_webhook=discord_info_webhook, err_webhook=discord_error_webhook, no_http=True)
+    return discord
+
+def test_error():
+    d = load_discord()
+    d.error('Normal error.')
+
+    try:
+        raise Exception("Error with exception.")
+    except Exception as e:
+        d.error(e)
+
+
+if __name__ == '__main__':
+    test_error()
--- tutorials/exceptions.py
+++ tutorials/exceptions.py
@@ -1,3 +1,4 @@
+import sys
 import traceback
 
 class MyException(Exception):
@@ -7,13 +8,39 @@ class MyException(Exception):
 def raise_my_error():
     raise MyException('abc')
 
-def test_stacktrace():
+
+def test_wrapper(n=5, limit=3):
+    def wrapper(n):
+        if n <=0:
+            raise_my_error()
+        else:
+            n -= 1
+            wrapper(n)
+
     try:
-        raise_my_error()
+        wrapper(n=n)
     except Exception as e:
         # print(e)
-        s = traceback.format_exc()
+        s = traceback.format_exc(limit=limit)
         print(s)
 
+def test_traceback():
+        s = traceback.format_exc(limit=5)
+        print(s)
 
-test_stacktrace()
+def test_sys_exc_info():
+    # _, _, tb = sys.exc_info()
+    # print(tb)
+    try:
+        raise_my_error()
+    except MyException as e:
+        _, _, tb = sys.exc_info()
+        lines = traceback.format_tb(tb)
+        print("".join(lines))
+        # traceback.print_tb(tb)
+        
+
+
+# test_traceback()
+# test_wrapper(n=10, limit=10)
+test_sys_exc_info()
