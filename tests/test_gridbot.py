c4102b2d8d78e81eae8a1cd5b9aef42e4e587143
Yuhao Wu <wyhfrank@gmail.com>
2021-07-06 17:21:08 +0000

    Update tests for grid_bot.

--- tests/test_gridbot.py
+++ tests/test_gridbot.py
@@ -1,23 +1,41 @@
 # https://realpython.com/pytest-python-testing/
 
+import sys
+sys.path.append('.')
+
 import requests
 import pytest
 import python_bitbankcc
 from grid_trade.base import GridBot
 from exchanges import Bitbank
+import logging
+from utils import setup_logging
+
+# Hmmm, logging not working in tests?
+setup_logging(level=logging.DEBUG)
+
+OrderStatus = Bitbank.OrderStatus
+
 
 source = {
-    'ticker': [
-        {
-        'last': 115,
-        'sell': 116,
-        'buy': 114,
-        },
+    'prices': [
+        10150,
+        10250,
+        10450,
     ],
     'get_orders_info':[
         {'orders': [
-            {'order_id':1002, 'status': 'FULLY_FILLED' },
-            # {'order_id':1001, 'status': 'UNFILLED' },
+            {'order_id': 2, 'status': OrderStatus.FullyFilled },
+            ],
+        },
+        {'orders': [
+            {'order_id': 3, 'status': OrderStatus.FullyFilled },
+            {'order_id': 4, 'status': OrderStatus.FullyFilled },
+            ],
+        },
+        {'orders': [
+            {'order_id': 5, 'status': OrderStatus.FullyFilled },
+            {'order_id': 8, 'status': OrderStatus.FullyFilled },
             ],
         },
     ],
@@ -30,27 +48,37 @@ class BitbankPublicMock:
 
     def get_ticker(self, pair):
         self.ticker_count += 1
-        return source['ticker'][self.ticker_count]
+        price = source['prices'][self.ticker_count]
+        ticker_data = {
+            'last': price,
+            'buy': price,
+            'sell': price,
+        }
+        return ticker_data
 
 class BitbankPrivateMock:
-    def __init__(self) -> None:
-        self.order_id = 999
+    def __init__(self, *args, **kwargs) -> None:
+        self.order_id = -1
         self.order_index = -1
 
     def order(self, pair, price, amount, side, order_type, post_only = False):
         self.order_id += 1
-        return {
+        order_data = {
             'order_id': self.order_id
             }
+        order_data['pair'] = 'eth_jpy'
+        order_data['ordered_at'] = 0
+        return order_data
 
     def get_orders_info(self, pair, order_ids):
         self.order_index += 1
-        # TODO
-        self.order_index = 0
-        return source['get_orders_info'][self.order_index]
+        orders_data = source['get_orders_info'][self.order_index]
+        for od in orders_data['orders']:
+            od['status'] = od['status'].value
+        return orders_data
 
     def cancel_orders(self, pair, order_ids):
-        pass
+        return {'orders': []}
 
 
 class TestGridBot:
@@ -68,7 +96,7 @@ class TestGridBot:
 
         prv = python_bitbankcc.private()
         res = prv.get_orders_info(pair='btc_jpy', order_ids=[])
-        assert res['orders'][0]['order_id'] == 1002
+        assert res['orders'][0]['order_id'] == 2
 
     def test_params(self, mock_bitbank):
         init_price = 100
@@ -81,74 +109,79 @@ class TestGridBot:
         params = GridBot.Parameter.calc_grid_params_by_support(init_base, init_quote, init_price, support, grid_num=grid_num, fee=fee)
         assert params.price_interval == 10
 
+    def test_bot(self, mock_bitbank):
+        init_price = 10000
+        init_quote = 700
+        init_base = 10
+        price_interval = 100
+        grid_num = 10
+        fee = -0.0002
+        pair = 'eth_jpy'
+
+        additional = {
+            "pair": pair,
+        }
+
+        param = GridBot.Parameter.calc_grid_params_by_interval(init_base, init_quote, init_price, 
+                                        price_interval=price_interval, pair=pair, grid_num=grid_num, fee=fee)
+        bitbank = Bitbank(pair=pair)
+        bitbank.max_order_count = 4
+
+        bot = GridBot(bitbank)
+        bot.init_and_start(param=param, additional_info=additional)
+
+        assert len(bot.om.active_orders) == 4
+
+        self.check_sides(bot, 2, 2)
+        self.check_prices(bot, [9800, 9900, 10100, 10200])
+        self.check_ids(bot, [1, 0, 2, 3])
+        
+        bot.om.balance_threshold = 2
+        bot.sync_order_status()
+        self.check_sides(bot, 2, 2)
+        self.check_prices(bot, [9900, 10000, 10200, 10300])
+        self.check_ids(bot, [0, 4, 3, 5])
+
+        bot.sync_order_status()
+        self.check_sides(bot, 2, 2)
+        self.check_prices(bot, [10000, 10100, 10300, 10400])
+        # 0 4 . 3 5
+        # 0 _   _.5
+        # 0 ? ? _.5
+        # x ? ? _.5 ?
+        #   7 6 _.5 8
+        self.check_ids(bot, [7, 6, 5, 8])
+
+        bot.sync_order_status()
+        self.check_sides(bot, 3, 2)
+        self.check_prices(bot, [10100, 10200, 10300, 10500, 10600])
+        # 7 6 . 5 8
+        # 7 6 . _ _
+        # 7 6 ? ? _.?
+        #   6 ? ? _.? ?
+        #   6 10 9 _.11 12
+        self.check_ids(bot, [6, 10, 9, 11, 12])
+
+
+    @staticmethod
+    def check_sides(bot, buy, sell):
+        exp_sides = ['Buy'] *  buy + ['Sell'] * sell
+        sides = [o.side.name for o in [*bot.om.buy_stack.active_orders, *bot.om.sell_stack.active_orders]]
+        assert sides == exp_sides
+
     @staticmethod
     def check_prices(bot, exp_prices):
         prices = sorted([o.price for o in bot.om.active_orders])
         assert prices == exp_prices        
 
     @staticmethod
-    def check_sides(bot, exp_sides):
-        sides = [o.side.name for o in [*bot.om.buy_stack.active_orders, *bot.om.sell_stack.active_orders]]
-        assert sides == exp_sides
-    
-    def test_bot(self, mock_bitbank):
-        init_price = 100
-        init_quote = 700
-        init_base = 10
-        support = 50
-        grid_num = 10
-        fee = -0.0002
-        pair = 'eth_jpy'
-
-        param = GridBot.Parameter.calc_grid_params_by_support(init_base, init_quote, init_price, support, grid_num=grid_num, fee=fee)
-        bitbank = Bitbank(pair=pair)
-        bitbank.max_order_count = 4
-
-        bot = GridBot(bitbank)
-        bot.init_and_start(param=param)
-
-        assert len(bot.om.active_orders) == 4
-
-        self.check_sides(bot, ['Buy'] * 2 + ['Sell'] * 2)
-        self.check_prices(bot, [80, 90, 110, 120])
-        
-        bot.om.balance_threshold = 0
-        bot.sync_order_status()
-
-        self.check_sides(bot, ['Buy'] * 2 + ['Sell'] * 2)
-        self.check_prices(bot, [90, 100, 120, 130])
-
-        
+    def check_ids(bot, exp_ids):
+        orders = sorted( bot.om.active_orders, key=lambda o: o.price)
+        ids = [o.order_id for o in orders]
+        assert ids == exp_ids
 
 
-
-    # def test_bot(self):
-    #     pair = 'eth_jpy'
-
-    #     init_price = 35393
-    #     init_quote = 50000
-    #     init_base = init_quote / init_price
-    #     support = 24756
-    #     fee = -0.0002
-    #     grid_num = 100
-
-    #     init_price = 100
-    #     init_quote = 700
-    #     init_base = 10
-    #     support = 50
-    #     grid_num = 10    
-
-    #     param = GridBot.Parameter.calc_grid_params_by_support(init_base, init_quote, init_price, support, grid_num=grid_num, fee=fee)
-
-    #     bitbank = Bitbank(pair=pair)
-    #     bot = GridBot(bitbank)
-    #     bot.init_and_start(param=param)
-
-    #     # print(bot.om.active_orders)
-    #     bot.om.print_stacks()
-
-    #     bot.sync_order_status()
-    #     bot.om.print_stacks()        
-        
-
-        
+if __name__ == '__main__':
+    # No idea why running this directly will fail the test
+    #   whereas, running by click the `Run Test` button (VSCode) in source code succeeds
+    retcode = pytest.main(['-x', __file__])
