c27c1baa0c72b708a3bff3fe3fa9d1f3b7209803
Yuhao Wu <wyhfrank@gmail.com>
2021-07-07 03:48:32 +0000

    Update test cases for order stack.

--- tests/test_order_stack.py
+++ tests/test_order_stack.py
@@ -41,11 +41,47 @@ class TestOrderStack:
 
         assert len(stack.all_orders) == 3
         assert stack.best_order.price == 9900
+        assert stack.worst_order.price == 9700
         assert [o.price for o in stack.all_orders] == [9900, 9800, 9700]
+        
         assert list(stack.get_price_grid(10000, direction='outer', count=3)) == [10000, 9900, 9800]
         assert list(stack.get_price_grid(10000, direction='inner', count=3)) == [10000, 10100, 10200]
         assert list(stack.get_price_grid(10002, direction='outer', count=3)) == [10000, 9900, 9800]
         assert list(stack.get_price_grid(9999, direction='inner', count=3)) == [10000, 10100, 10200]
+
+        stack.refill_orders(direction='outer')
+        assert [o.price for o in stack.all_orders] == [9900, 9800, 9700, 9600]
+
+        stack.refill_orders(direction='inner')
+        assert [o.price for o in stack.all_orders] == [10000, 9900, 9800, 9700, 9600]
+        
+        stack.refill_orders(direction='inner', count=2)
+        assert [o.price for o in stack.all_orders] == [10200, 10100, 10000, 9900, 9800, 9700, 9600]
+        
+    def test_sell(self):
+        init_price = 10000
+
+        stack = self.om.sell_stack
+        stack.prepare_init(init_price=init_price)
+
+        assert len(stack.all_orders) == 3
+        assert stack.best_order.price == 10100
+        assert stack.worst_order.price == 10300
+        assert [o.price for o in stack.all_orders] == [10100, 10200, 10300]
+        
+        assert list(stack.get_price_grid(10000, direction='outer', count=3)) == [10000, 10100, 10200]
+        assert list(stack.get_price_grid(10000, direction='inner', count=3)) == [10000, 9900, 9800]
+        assert list(stack.get_price_grid(10002, direction='outer', count=3)) == [10100, 10200, 10300]
+        assert list(stack.get_price_grid(9999, direction='inner', count=3)) == [9900, 9800, 9700]
+
+        stack.refill_orders(direction='outer')
+        assert [o.price for o in stack.all_orders] == [10100, 10200, 10300, 10400]
+
+        stack.refill_orders(direction='inner')
+        assert [o.price for o in stack.all_orders] == [10000, 10100, 10200, 10300, 10400]
+        
+        stack.refill_orders(direction='inner', count=2)
+        assert [o.price for o in stack.all_orders] == [9800, 9900, 10000, 10100, 10200, 10300, 10400]
         
 
 
