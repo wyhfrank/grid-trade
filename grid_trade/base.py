59526afb3ad56cdb8a11e58523edc9ac778a9c42
Yuhao Wu <wyhfrank@gmail.com>
2021-07-13 16:59:49 +0000

    Bump up version: 0.1.6

--- grid_trade/base.py
+++ grid_trade/base.py
@@ -15,7 +15,7 @@ from utils import format_float, format_rate, init_formatted_properties
 
 logger = logging.getLogger(__name__)
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 
 class BotStatus(Enum):
